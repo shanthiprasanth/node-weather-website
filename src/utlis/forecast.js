de2cb7a216dd3449ca145fd84bad6184778b01eb
shanthi prasanth <shandharani678@gmail.com>
2020-10-10 10:44:23 +0530

    Humidity added

--- src/utlis/forecast.js
+++ src/utlis/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. Its feels like ' + body.current.feelslike + ' degress out')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. Its feels like ' + body.current.feelslike + ' degress out. The Humidity is ' + body.current.humidity + ' °F')
         }
     })
 }
